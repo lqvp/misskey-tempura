69afcce4e60243e4e2473cdca2d1952884c951b9
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-03-08 02:09:23 +0900

    feat(client): フォントを変更できる機能を実装

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -2113,6 +2113,10 @@ export interface Locale extends ILocale {
      */
     "fontSize": string;
     /**
+     * カスタムフォント
+     */
+    "customFont": string;
+    /**
      * 画像が1枚のみのメディアリストの高さ
      */
     "mediaListWithOneImageAppearance": string;
