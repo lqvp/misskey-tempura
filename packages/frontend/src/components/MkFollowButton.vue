2c4166c2b70ec43f2f013e9d6108c2f73221f2cb
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2024-03-21 00:34:59 +0900

    enhance(frontend): フォロワーの通知にフォローバックボタンを追加 (MisskeyIO#552)

--- packages/frontend/src/components/MkFollowButton.vue
+++ packages/frontend/src/components/MkFollowButton.vue
@@ -6,25 +6,25 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <button
 	class="_button"
-	:class="[$style.root, { [$style.wait]: wait, [$style.active]: isFollowing || hasPendingFollowRequestFromYou, [$style.full]: full, [$style.large]: large }]"
+	:class="[$style.root, { [$style.wait]: wait, [$style.active]: userDetailed.isFollowing || userDetailed.hasPendingFollowRequestFromYou, [$style.full]: full, [$style.large]: large }]"
 	:disabled="wait"
 	@click="onClick"
 >
 	<template v-if="!wait">
-		<template v-if="hasPendingFollowRequestFromYou && user.isLocked">
+		<template v-if="userDetailed.hasPendingFollowRequestFromYou && userDetailed.isLocked">
 			<span v-if="full" :class="$style.text">{{ i18n.ts.followRequestPending }}</span><i class="ti ti-hourglass-empty"></i>
 		</template>
-		<template v-else-if="hasPendingFollowRequestFromYou && !user.isLocked">
+		<template v-else-if="userDetailed.hasPendingFollowRequestFromYou && !userDetailed.isLocked">
 			<!-- つまりリモートフォローの場合。 -->
 			<span v-if="full" :class="$style.text">{{ i18n.ts.processing }}</span><MkLoading :em="true" :colored="false"/>
 		</template>
-		<template v-else-if="isFollowing">
+		<template v-else-if="userDetailed.isFollowing">
 			<span v-if="full" :class="$style.text">{{ i18n.ts.unfollow }}</span><i class="ti ti-minus"></i>
 		</template>
-		<template v-else-if="!isFollowing && user.isLocked">
+		<template v-else-if="!userDetailed.isFollowing && userDetailed.isLocked">
 			<span v-if="full" :class="$style.text">{{ i18n.ts.followRequest }}</span><i class="ti ti-plus"></i>
 		</template>
-		<template v-else-if="!isFollowing && !user.isLocked">
+		<template v-else-if="!userDetailed.isFollowing && !userDetailed.isLocked">
 			<span v-if="full" :class="$style.text">{{ i18n.ts.follow }}</span><i class="ti ti-plus"></i>
 		</template>
 	</template>
@@ -49,7 +49,7 @@ import { defaultStore } from '@/store.js';
 import {userName} from "@/filters/user";
 
 const props = withDefaults(defineProps<{
-	user: Misskey.entities.UserDetailed,
+	user: { id: string } & Partial<Misskey.entities.UserDetailed>,
 	full?: boolean,
 	large?: boolean,
 }>(), {
@@ -57,26 +57,27 @@ const props = withDefaults(defineProps<{
 	large: false,
 });
 
-const emit = defineEmits<{
-	(_: 'update:user', value: Misskey.entities.UserDetailed): void
-}>();
+const emit = defineEmits<
+	(_: 'update:user', value: Misskey.entities.UserDetailed) => void
+>();
 
-const isFollowing = ref(props.user.isFollowing);
-const hasPendingFollowRequestFromYou = ref(props.user.hasPendingFollowRequestFromYou);
-const wait = ref(false);
+const userDetailed = ref<{ id: string } & Partial<Misskey.entities.UserDetailed>>(props.user);
+const wait = ref(props.user.isLocked === undefined);
 const connection = useStream().useChannel('main');
 
-if (props.user.isFollowing == null && $i) {
+if (userDetailed.value.isLocked === undefined && $i) {
 	misskeyApi('users/show', {
 		userId: props.user.id,
 	})
-		.then(onFollowChange);
+		.then(onFollowChange)
+		.then(() => {
+			wait.value = false;
+		});
 }
 
 function onFollowChange(user: Misskey.entities.UserDetailed) {
 	if (user.id === props.user.id) {
-		isFollowing.value = user.isFollowing;
-		hasPendingFollowRequestFromYou.value = user.hasPendingFollowRequestFromYou;
+		userDetailed.value = user;
 	}
 }
 
@@ -86,10 +87,10 @@ async function onClick() {
 	wait.value = true;
 
 	try {
-		if (isFollowing.value) {
+		if (userDetailed.value.isFollowing) {
 			const { canceled } = await os.confirm({
 				type: 'warning',
-				text: i18n.tsx.unfollowConfirm({ name: props.user.name || props.user.username }),
+				text: i18n.tsx.unfollowConfirm({ name: (userDetailed.value.name || userDetailed.value.username) ?? i18n.ts.user }),
 			});
 
 			if (canceled) return;
@@ -110,37 +111,39 @@ async function onClick() {
 				}
 			}
 
-			if (hasPendingFollowRequestFromYou.value) {
+			if (userDetailed.value.hasPendingFollowRequestFromYou) {
 				await misskeyApi('following/requests/cancel', {
 					userId: props.user.id,
 				});
-				hasPendingFollowRequestFromYou.value = false;
+				userDetailed.value.hasPendingFollowRequestFromYou = false;
 			} else {
 				await misskeyApi('following/create', {
 					userId: props.user.id,
 					withReplies: defaultStore.state.defaultWithReplies,
 				});
 				emit('update:user', {
-					...props.user,
+					...userDetailed.value,
 					withReplies: defaultStore.state.defaultWithReplies,
 				});
-				hasPendingFollowRequestFromYou.value = true;
+				userDetailed.value.hasPendingFollowRequestFromYou = true;
 
 				if ($i == null) return;
 
-				claimAchievement('following1');
+				if ($i) {
+					claimAchievement('following1');
 
-				if ($i.followingCount >= 10) {
-					claimAchievement('following10');
-				}
-				if ($i.followingCount >= 50) {
-					claimAchievement('following50');
-				}
-				if ($i.followingCount >= 100) {
-					claimAchievement('following100');
-				}
-				if ($i.followingCount >= 300) {
-					claimAchievement('following300');
+					if ($i.followingCount >= 10) {
+						claimAchievement('following10');
+					}
+					if ($i.followingCount >= 50) {
+						claimAchievement('following50');
+					}
+					if ($i.followingCount >= 100) {
+						claimAchievement('following100');
+					}
+					if ($i.followingCount >= 300) {
+						claimAchievement('following300');
+					}
 				}
 			}
 		}
@@ -190,15 +193,17 @@ onBeforeUnmount(() => {
 	}
 
 	&:focus-visible {
-		outline-offset: 2px;
-	}
-
-	&:hover {
-		//background: mix($primary, #fff, 20);
-	}
-
-	&:active {
-		//background: mix($primary, #fff, 40);
+		&:after {
+			content: "";
+			pointer-events: none;
+			position: absolute;
+			top: -5px;
+			right: -5px;
+			bottom: -5px;
+			left: -5px;
+			border: 2px solid var(--focus);
+			border-radius: 32px;
+		}
 	}
 
 	&.active {
