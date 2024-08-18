8532e243c65ded157858cf1992473cdf12c79a62
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-08-18 22:33:19 +0900

    fix: 独自機能に関する項目がバックアップされていなかった問題を修正

--- packages/frontend/src/pages/settings/preferences-backups.vue
+++ packages/frontend/src/pages/settings/preferences-backups.vue
@@ -111,6 +111,15 @@ const defaultStoreSaveKeys: (keyof typeof defaultStore['state'])[] = [
 	'sound_note',
 	'sound_noteMy',
 	'sound_notification',
+	'collapseRenotesTrigger',
+	'collapseSelfRenotes',
+	'defaultScheduledNoteDelete',
+	'defaultScheduledNoteDeleteTime',
+	'postFormActions',
+	'customFont',
+	'instanceTickerStyle',
+	'hideReactionCount',
+	'hideReactionUsers',
 ];
 const coldDeviceStorageSaveKeys: (keyof typeof ColdDeviceStorage.default)[] = [
 	'lightTheme',
