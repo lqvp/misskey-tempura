f2bd65280e401a3c0cf667d97173c128e768a570
nr1a <_@nr1a.inc>
2024-08-21 08:11:11 +0900

    fix

--- packages/frontend/src/pages/about.vue
+++ packages/frontend/src/pages/about.vue
@@ -27,7 +27,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 import { computed, defineAsyncComponent, ref, watch } from 'vue';
 import { i18n } from '@/i18n.js';
 // import { claimAchievement } from '@/scripts/achievements.js';
-// import { definePageMetadata } from '@/scripts/page-metadata.js';
+import { definePageMetadata } from '@/scripts/page-metadata.js';
 import MkHorizontalSwipe from '@/components/MkHorizontalSwipe.vue';
 
 const XOverview = defineAsyncComponent(() => import('@/pages/about.overview.vue'));
