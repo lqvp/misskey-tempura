a1eea4cad15d5776bc2ef7ba9152dd3fe0a9f151
nr1a <_@nr1a.inc>
2024-08-21 08:09:03 +0900

    これで良くない？

--- packages/frontend/src/pages/about.vue
+++ packages/frontend/src/pages/about.vue
@@ -7,16 +7,16 @@ SPDX-License-Identifier: AGPL-3.0-only
 <MkStickyContainer>
 	<template #header><MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
 	<MkHorizontalSwipe v-model:tab="tab" :tabs="headerTabs">
-		<MkSpacer v-if="tab === 'overview'" :contentMax="600" :marginMin="20">
+		<MkSpacer v-if="tab === 'overview'" :contentMax="1000" :marginMin="20">
 			<XOverview/>
 		</MkSpacer>
-		<MkSpacer v-else-if="tab === 'emojis'" :contentMax="1000" :marginMin="20">
+		<!-- <MkSpacer v-else-if="tab === 'emojis'" :contentMax="1000" :marginMin="20">
 			<XEmojis/>
 		</MkSpacer>
 		<MkSpacer v-else-if="tab === 'federation'" :contentMax="1000" :marginMin="20">
 			<XFederation/>
 		</MkSpacer>
-		<!-- <MkSpacer v-else-if="tab === 'charts'" :contentMax="1000" :marginMin="20">
+		<MkSpacer v-else-if="tab === 'charts'" :contentMax="1000" :marginMin="20">
 			<MkInstanceStats/>
 		</MkSpacer> -->
 	</MkHorizontalSwipe>
@@ -26,13 +26,13 @@ SPDX-License-Identifier: AGPL-3.0-only
 <script lang="ts" setup>
 import { computed, defineAsyncComponent, ref, watch } from 'vue';
 import { i18n } from '@/i18n.js';
-import { claimAchievement } from '@/scripts/achievements.js';
+// import { claimAchievement } from '@/scripts/achievements.js';
 // import { definePageMetadata } from '@/scripts/page-metadata.js';
 import MkHorizontalSwipe from '@/components/MkHorizontalSwipe.vue';
 
 const XOverview = defineAsyncComponent(() => import('@/pages/about.overview.vue'));
-const XEmojis = defineAsyncComponent(() => import('@/pages/about.emojis.vue'));
-const XFederation = defineAsyncComponent(() => import('@/pages/about.federation.vue'));
+// const XEmojis = defineAsyncComponent(() => import('@/pages/about.emojis.vue'));
+// const XFederation = defineAsyncComponent(() => import('@/pages/about.federation.vue'));
 // const MkInstanceStats = defineAsyncComponent(() => import('@/components/MkInstanceStats.vue'));
 
 const props = withDefaults(defineProps<{
@@ -43,30 +43,35 @@ const props = withDefaults(defineProps<{
 
 const tab = ref(props.initialTab);
 
-watch(tab, () => {
-	if (tab.value === 'charts') {
-		claimAchievement('viewInstanceChart');
-	}
-});
+// watch(tab, () => {
+// 	if (tab.value === 'charts') {
+// 		claimAchievement('viewInstanceChart');
+// 	}
+// });
 
 const headerActions = computed(() => []);
 
 const headerTabs = computed(() => [{
 	key: 'overview',
 	title: i18n.ts.overview,
-}, {
-	key: 'emojis',
-	title: i18n.ts.customEmojis,
-	icon: 'ti ti-icons',
-}, {
-	key: 'federation',
-	title: i18n.ts.federation,
-	icon: 'ti ti-whirl',
-}, {
-	// key: 'charts',
-	// title: i18n.ts.charts,
-	// icon: 'ti ti-chart-line',
 }]);
+
+// const headerTabs = computed(() => [{
+// 	key: 'overview',
+// 	title: i18n.ts.overview,
+// }, {
+// 	key: 'emojis',
+// 	title: i18n.ts.customEmojis,
+// 	icon: 'ti ti-icons',
+// }, {
+// 	key: 'federation',
+// 	title: i18n.ts.federation,
+// 	icon: 'ti ti-whirl',
+// }, {
+// 	key: 'charts',
+// 	title: i18n.ts.charts,
+// 	icon: 'ti ti-chart-line',
+// }]);
 
 definePageMetadata(() => ({
 	title: i18n.ts.instanceInfo,
