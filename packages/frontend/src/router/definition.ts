4134fd4d0d2f6a877e3633ea1079a54435ec0360
lqvp <183242690+lqvp@users.noreply.github.com>
2024-10-11 19:36:29 +0900

    かくす

--- packages/frontend/src/router/definition.ts
+++ packages/frontend/src/router/definition.ts
@@ -34,6 +34,7 @@ const routes: RouteDef[] = [{
 	name: 'user',
 	path: '/@:acct/:page?',
 	component: page(() => import('@/pages/user/index.vue')),
+	loginRequired: true,
 }, {
 	name: 'note',
 	path: '/notes/:noteId/:initialTab?',
