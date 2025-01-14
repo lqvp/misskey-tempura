dfd3487dda54a35b02c9d492fb29a2681677b5f5
lqvp <183242690+lqvp@users.noreply.github.com>
2025-01-14 23:14:30 +0900

    Revert "feat: pgroongaの対応"
    This reverts commit 6c12c8bad799cf6a97a81736cd1f227474614114.

--- packages/backend/src/models/UserProfile.ts
+++ packages/backend/src/models/UserProfile.ts
@@ -43,7 +43,6 @@ export class MiUserProfile {
 	})
 	public listenbrainz: string | null;
 
-	@Index() // USING pgroonga pgroonga_varchar_full_text_search_ops_v2
 	@Column('varchar', {
 		length: 2048, nullable: true,
 		comment: 'The description (bio) of the User.',
