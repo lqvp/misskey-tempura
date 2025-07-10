07d24f70a7ea7d1c4743026599be7c8b47c63862
lqvp <183242690+lqvp@users.noreply.github.com>
2025-07-10 12:49:53 +0900

    fix(migration): nameの記入漏れ

--- packages/backend/migration/1751980434169-replaceGithubLinks.js
+++ packages/backend/migration/1751980434169-replaceGithubLinks.js
@@ -4,6 +4,8 @@
  */
 
 export class ReplaceGithubLinks1751980434169 {
+	name = 'ReplaceGithubLinks1751980434169'
+	
 	async up(queryRunner) {
 			await queryRunner.query(`UPDATE "meta" SET "repositoryUrl" = 'https://github.com/lqvp/misskey-tempura' WHERE "repositoryUrl" = 'https://github.com/misskey-dev/misskey'`);
 			await queryRunner.query(`UPDATE "meta" SET "feedbackUrl" = 'https://github.com/lqvp/misskey-tempura/issues/new' WHERE "feedbackUrl" = 'https://github.com/misskey-dev/misskey/issues/new'`);
