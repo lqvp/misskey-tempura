455ef0a71547c23e9bf753986fc334e9505bef81
lqvp <183242690+lqvp@users.noreply.github.com>
2026-05-11 21:17:08 +0900

    Xfix(backend): エスケープの考慮を追加 #568

--- packages/backend/src/server/web/ClientServerService.ts
+++ packages/backend/src/server/web/ClientServerService.ts
@@ -41,6 +41,7 @@ import { bindThis } from '@/decorators.js';
 import { FlashEntityService } from '@/core/entities/FlashEntityService.js';
 import { ReversiGameEntityService } from '@/core/entities/ReversiGameEntityService.js';
 import { AnnouncementEntityService } from '@/core/entities/AnnouncementEntityService.js';
+import { escapeValue } from '@/misc/prelude/xml.js';
 import { FeedService } from './FeedService.js';
 import { UrlPreviewService } from './UrlPreviewService.js';
 import { ClientLoggerService } from './ClientLoggerService.js';
@@ -420,13 +421,15 @@ export class ClientServerService {
 		// OpenSearch XML
 		fastify.get('/opensearch.xml', async (request, reply) => {
 			const name = this.meta.name ?? 'Misskey';
+			const escapedName = escapeValue(name);
+			const escapedUrl = escapeValue(this.config.url);
 			let content = '';
 			content += '<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">';
-			content += `<ShortName>${name}</ShortName>`;
-			content += `<Description>${name} Search</Description>`;
+			content += `<ShortName>${escapedName}</ShortName>`;
+			content += `<Description>${escapedName} Search</Description>`;
 			content += '<InputEncoding>UTF-8</InputEncoding>';
-			content += `<Image width="16" height="16" type="image/x-icon">${this.config.url}/favicon.ico</Image>`;
-			content += `<Url type="text/html" template="${this.config.url}/search?q={searchTerms}"/>`;
+			content += `<Image width="16" height="16" type="image/x-icon">${escapedUrl}/favicon.ico</Image>`;
+			content += `<Url type="text/html" template="${escapedUrl}/search?q={searchTerms}"/>`;
 			content += '</OpenSearchDescription>';
 
 			reply.header('Content-Type', 'application/opensearchdescription+xml');
